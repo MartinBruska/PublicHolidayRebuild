776390df484f8fb6838feb70be1b1e2f66f46e17
Martin Bruska <martin.bruska@accesshq.com>
2020-02-10 15:22:24 +1100

    main.dart cleaning

--- public_holiday/lib/main.dart
+++ public_holiday/lib/main.dart
@@ -9,12 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Public Holiday',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: HomePage(),
     );
   }
 }
-
-
