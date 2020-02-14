4f16080ecbaecad86cbe44095bb8bcb3ee9c7452
Martin Bruska <martin.bruska@accesshq.com>
2020-02-15 06:07:05 +1100

    bottom app bar initialized

---
+++ public_holiday/lib/components/bottom_app_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BottomAppBar extends StatelessWidget{
+  final List<> icons;
+  final List<String> icons_title;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
