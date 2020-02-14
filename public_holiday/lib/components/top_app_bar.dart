1fbf8ff32d5679c23c46702d20bb8002d13f11fd
Martin Bruska <martin.bruska@accesshq.com>
2020-02-15 05:27:40 +1100

    second part app bar rebuild to container, margin to app bar added

--- public_holiday/lib/components/top_app_bar.dart
+++ public_holiday/lib/components/top_app_bar.dart
@@ -44,6 +44,7 @@ class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
                 ),
                 width: MediaQuery.of(context).size.width * 0.6,
                 height: 50,
+                margin: const EdgeInsets.all(8.0),
                 child: Row(
                   children: <Widget>[
                     Padding(
@@ -61,38 +62,49 @@ class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
                   ],
                 ),
               ),
-              Card(
-                color: Colors.yellow,
-                elevation: 10,
-                shape: RoundedRectangleBorder(
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFDA44),
                   borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
                     bottomRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(30),
                   ),
+                  gradient: LinearGradient(
+                    begin: Alignment(0, 0.5),
+                    end: Alignment.topCenter,
+                    colors: [const Color(0xFFFFDA44), const Color(0xFFFFFFFF)],
+                    tileMode: TileMode.clamp,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.25),
+                      offset: Offset(1, 6),
+                      blurRadius: 8.0,
+                    ),
+                  ],
                 ),
-                child: Container(
-                  height: 50,
-                  width: MediaQuery.of(context).size.width * 0.30,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      GestureDetector(
-                        onTap: () {},
-                        child: const Icon(
-                          Icons.notifications,
-                          size: 30,
-                        ),
+                height: 50,
+                width: MediaQuery.of(context).size.width * 0.30,
+                margin: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Icon(
+                        Icons.notifications,
+                        size: 30,
                       ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: const Icon(
-                          Icons.more_vert,
-                          size: 30,
-                        ),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Icon(
+                        Icons.more_vert,
+                        size: 30,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ],
