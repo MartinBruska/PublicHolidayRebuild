c81968145d27f2d4518027ef16cc28f0c323d20c
Martin Bruska <martin.bruska@accesshq.com>
2020-02-23 21:14:25 +1100

    bottomAppbar colors changed, readme design ad

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PublicHolidayRebuild
 Flutter Rebuild of Public Holiday App
+
+![Redesign PublicHoliday](/design.jpg?raw=true "Redesign Public Holiday")
--- public_holiday/lib/components/bottom_app_bar.dart
+++ public_holiday/lib/components/bottom_app_bar.dart
@@ -9,11 +9,43 @@ class CustomBottomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          BottomAppBarButton(
+            iconName: "countries_icon.png",
+            title: "Countries",
+            onTapFunction: () {
+              print(" countries tapped");
+            },
+          ),
+          BottomAppBarButton(
+            iconName: "favs_icon.png",
+            title: "Favs",
+            onTapFunction: () {
+              print(" favs tapped");
+            },
+          ),
+          BottomAppBarButton(
+            iconName: "recent_icon.png",
+            title: "Recent",
+            onTapFunction: () {
+              print(" recent tapped");
+            },
+          ),
+          BottomAppBarButton(
+            iconName: "countries_search_icon.png",
+            title: "Search",
+            onTapFunction: () {
+              print(" search tapped");
+            },
+          ),
+        ],
+      ),
       height: 66.0,
       width: MediaQuery.of(context).size.width,
       margin: EdgeInsets.all(8.0),
       decoration: BoxDecoration(
-        color: const Color(0xFFFFDA44),
         borderRadius: BorderRadius.only(
           topRight: Radius.circular(20),
           topLeft: Radius.circular(20),
@@ -35,65 +67,60 @@ class CustomBottomAppBar extends StatelessWidget {
           ),
         ],
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          BottomAppBarButton(
-            iconName: "countries_icon.png",
-            title: "Countries",
-            onTapFunction: null,
-          ),
-          BottomAppBarButton(
-            iconName:"favs_icon.png",
-            title: "Favs",
-            onTapFunction: null,
-          ),
-          BottomAppBarButton(
-            iconName:"recent_icon.png",
-            title: "Recent",
-            onTapFunction: null,
-          ),
-          BottomAppBarButton(
-            iconName: "countries_search_icon.png",
-            title: "Search",
-            onTapFunction: null,
-          ),
-        ],
-      ),
     );
   }
 }
 
-class BottomAppBarButton extends StatelessWidget {
+class BottomAppBarButton extends StatefulWidget {
   final String iconName;
   final String title;
-  final Function onTapFunction;
+  final VoidCallback onTapFunction;
 
   BottomAppBarButton(
       {@required this.iconName,
       @required this.title,
       @required this.onTapFunction});
+
+  @override
+  _BottomAppBarButtonState createState() => _BottomAppBarButtonState();
+}
+
+class _BottomAppBarButtonState extends State<BottomAppBarButton> {
+  bool _pressed = false;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: this.onTapFunction,
-      child: Container(
-        width: 50,
-        height: 55,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-              child: Image(image: AssetImage("assets/icons/${this.iconName}")),
-            ),
-            Text(
-              this.title,
-              style: GoogleFonts.lobster(
-                fontSize: 14,
+    return Material(
+      //type: MaterialType.transparency,
+      color: _pressed ? Color(0xFFFFDA44) : Colors.transparent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(10.0),
+        onTap: () {
+          widget.onTapFunction();
+          this.setState(() {
+            _pressed = true;
+          });
+        },
+        //highlightColor: Colors.green,
+        child: Container(
+          padding: EdgeInsets.all(4.0),
+          width: 70,
+          height: 66,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: Image(
+                    image: AssetImage("assets/icons/${this.widget.iconName}")),
               ),
-            )
-          ],
+              Text(
+                this.widget.title,
+                style: GoogleFonts.lobster(
+                  fontSize: 14,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
