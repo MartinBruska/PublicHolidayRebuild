216c3f2d1af467d2fae8ac7bf39848dee3de56a3
Martin Bruska <martin.bruska@accesshq.com>
2020-03-30 21:58:16 +1100

    country list tile added

--- public_holiday/lib/screens/countries_page/countries_page.dart
+++ public_holiday/lib/screens/countries_page/countries_page.dart
@@ -4,13 +4,21 @@ import 'package:public_holiday/screens/countries_page/country_list_tile.dart';
 class CountriesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: CountryListTile(
-        countryFlagImage: Image(
-          image: AssetImage("assets/images/turkey-flag.jpg"),
+    return Column(
+      children: <Widget>[
+        CountryListTile(
+          countryFlagImage: AssetImage(
+            "assets/images/turkey-flag.jpg",
+          ),
+          countryName: "Turkey",
         ),
-        countryName: "Turkey",
-      ),
+        CountryListTile(
+          countryFlagImage: AssetImage(
+            "assets/images/turkey-flag.jpg",
+          ),
+          countryName: "Turkey",
+        ),
+      ],
     );
   }
 }
--- public_holiday/lib/screens/countries_page/country_list_tile.dart
+++ public_holiday/lib/screens/countries_page/country_list_tile.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CountryListTile extends StatelessWidget {
-  final Image countryFlagImage;
+  final AssetImage countryFlagImage;
   final String countryName;
 
   CountryListTile(
@@ -9,11 +10,36 @@ class CountryListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: this.countryFlagImage,
-        title: Text(this.countryName),
-        trailing: Icon(Icons.favorite_border),
+    return Container(
+      height: 80,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.only(
+        left: 4.0,
+        right: 4.0,
+      ),
+      child: Card(
+        elevation: 8,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        )),
+        child: Center(
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: this.countryFlagImage,
+              radius: 27,
+            ),
+            title: Text(
+              this.countryName,
+              style: GoogleFonts.patuaOne(fontSize: 25),
+            ),
+            trailing: Icon(
+              Icons.favorite,
+              size: 30,
+              color: Colors.red,
+            ),
+          ),
+        ),
       ),
     );
   }
